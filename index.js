65ae9278f85f5b618ef63753a1d976f93e0bea52
fadiyaldo <Fadi_baza@yahoo.com>
2022-10-15 23:45:13 -0400

    running server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+
+dotenv.config();
+mongoose
+.connect (process.env.MONGO_URL
+).then(()=> console.log("connection is successfull"))
+.catch((err)=>{
+    console.log(err);
+});
+app.listen(5000, ()=> {
+    console.log("Backend server is running");
+});
